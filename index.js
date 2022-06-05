f2e40914f854a6438ca51ad7b7c152f50bc3f9d3
Dark Tower <prakashvishaal@gmail.com>
2022-06-05 01:27:13 -0600

    completed

---
+++ index.js
@@ -0,0 +1,246 @@
+// Required packages 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./src/generateHTML');
+// My libraries
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// creating team array
+let teamArr = [];
+
+// function to add Manager to team
+const addManager = () => {
+     return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter MANAGER name:',
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                    return true;
+                } else {
+                    console.log('You MUST to enter a name to continue!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee id for MANAGER:',
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter a employee ID to continue!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter MANAGER email address:',
+            validate: emailAddressInput => {
+                if (emailAddressInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter an email address to continue!');
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "officeNum",
+            message: "Enter MANAGER office number:",
+            validate: officeNumInput => {
+                if (officeNumInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter an office number to continue!');
+                }
+            }
+        },
+
+    ])
+        .then(function (managerInput) {
+            const { name, id, email, officeNum } = managerInput;
+            const manager = new Manager(name, id, email, officeNum);
+
+            teamArr.push(manager);
+            console.log(teamArr);
+            addTeamMember();
+        })
+
+
+};
+// function to  add team member
+const addTeamMember = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "addTeam",
+            message: "What would you like to do next? Please CHOOSE 1",
+            choices: [
+                "Add Engineer",
+                "Add Intern",
+                "Finish Building My Team"
+            ]
+        }
+    ]).then(function (data) {
+        switch (data.addTeam) {
+            case "Add Engineer":
+                addEngineer();
+                break;
+
+            case "Add Intern":
+                addIntern();
+                break;
+
+            case "Finish Building My Team":
+                createTeam(teamArr);
+                break;
+        }
+    });
+};
+// function to add engineer
+const addEngineer = () => {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engName',
+            message: 'Enter the engineer name:',
+            validate: engNameInput => {
+                if (engNameInput) {
+                    return true;
+                } else {
+                    console.log('You must enter engineer name to continue!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee id for ENGINEER:',
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter an employee ID to continue!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter ENGINEER email address:',
+            validate: emailAddressInput => {
+                if (emailAddressInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter an email address to continue!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter ENGINEER github username:',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter a github username to continue!');
+                }
+            }
+        }
+    ]).then(function (engInput) {
+        const { engName, id, email, github } = engInput;
+        const engineer = new Engineer(engName, id, email, github);
+
+        teamArr.push(engineer);
+        console.log(teamArr);
+        addTeamMember();
+    });
+};
+// function to add  intern
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'Enter the INTERN name:',
+            validate: internNameInput => {
+                if (internNameInput) {
+                    return true;
+                } else {
+                    console.log('You must enter intern name to continue!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee id for INTERN:',
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter an employee ID to continue!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter intern email address:',
+            validate: emailAddressInput => {
+                if (emailAddressInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter an email address to continue!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter INTERN school:',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('You MUST enter a SCHOOL to continue!');
+                }
+            }
+        }
+    ]).then(function (internInput) {
+        const { internName, id, email, school } = internInput;
+        const intern = new Intern(internName, id, email, school);
+
+        teamArr.push(intern);
+        console.log(teamArr);
+        addTeamMember();
+    });
+};
+
+const createTeam = (teamArr) => {
+    writeFile('./dist/index.html', generateHTML(teamArr));
+    console.log(`You're finished building your team! Check out index.html to see your complete roster!`);
+    ;
+};
+
+const writeFile = (filename, data) => {
+    fs.writeFile(filename, data, (err) => {
+        if(err)
+        throw(err);
+        
+    });
+};
+ 
+// start app 
+addManager();
+
---
+++ src/generateHTML.js
@@ -0,0 +1,65 @@
+
+
+const generateHTML = (teamArr) => {
+    
+   let object = '';
+    for(let i =0; i<teamArr.length; i++){
+        object += `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${teamArr[i].role}</h5>
+          <h6 class="card-subtitle mb-2 ">${teamArr[i].name}</h6>
+          <p class="card-text">Employee id : ${teamArr[i].id} </p>
+          <p class="card-text">Email: <a href="mailto:${teamArr[i].email}" class="card-link">${teamArr[i].email}</a><p>
+
+        
+        `
+        if (teamArr[i].officeNumber){
+            object +=`
+            <p class="card-text">Office Number: ${teamArr[i].officeNumber} </p>
+            `
+        }
+        if (teamArr[i].github) {
+            object += `
+            <a href="https://www.github.com/${teamArr[i].github}" target=_blank class="card-link">Github: ${teamArr[i].github}</a><br>
+            `
+        }
+        if(teamArr[i].school) {
+            object += `
+            <p class="card-text">School: ${teamArr[i].school} </p>
+            `
+        }
+        object+= 
+        `</div>
+        </div>`
+    }
+        return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Roster</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <header><h1> Team Roster </h1></header>
+    <div class="app">
+    ${object}
+</div>
+    
+</body>
+</html>
+    
+    `
+        
+    
+    
+    
+}
+module.exports = generateHTML;
